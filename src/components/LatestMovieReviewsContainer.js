d983926be0d5a552125ab733806c9024109d3cfa
Zac <zacharyflynn06@gmail.com>
2021-07-15 11:36:35 -0500

    commit tree 8588734ffed19f85586d433f5a0de5b77d1a0c61

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,39 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = '7J62YPjItuezMnCUfTGJyTbGmhEoRroJ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            reviews: []
+         }
+    }
+    
+    componentDidMount() {
+        fetch(URL)
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                    reviews: json.results
+                })
+                debugger
+            })
+    }
+
+    
+    render() { 
+        return ( 
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+         );
+    }
+}
+ 
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,27 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = '7J62YPjItuezMnCUfTGJyTbGmhEoRroJ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            reviews: [],
+            searchTerm: ""
+         }
+    }
+    render() { 
+        return ( 
+            <div className="searchable-movie-reviews">
+
+            </div>
+         );
+    }
+}
+ 
+export default SearchableMovieReviewsContainer;
